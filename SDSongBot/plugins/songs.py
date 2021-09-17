67c7dad94648b64fb7825235bd46aed685989f2d
Dewmith1 <90494258+Dewmith1@users.noreply.github.com>
2021-09-17 07:55:07 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -26,7 +26,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song... @IMkashyapaa')
+    m = message.reply('🔎 Finding the song...@IMkashyapaa')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**~ 😎Uploaded by @IMkashyapaa🥳 [{}](tg://user?id=),Love you❤️ ~**'
+        rep = '**~ 🤖Uploaded by @IMkashyapaa🤖 (tg://user?id=),Love you❤️ ~**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
