530686806a3325c4f583e2d88e4a64e2fe4a884f
Kashyapa Dewmith <90494258+Dewmith1@users.noreply.github.com>
2021-09-29 08:30:29 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading the song by @IMkashyapaa 📥")
+    m.edit("📥🎧Downloading the song by @IMkashyapaa🎧📥")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**~ 🤖Uploaded by @IMkashyapaa🤖 ❤️Thank for using❤️~**'
+        rep = '**~🤍Uploaded by @IMkashyapaa🤍 🎵Thank for using🎵~**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
